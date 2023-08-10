8f23433e06d40f29c21e54476ba1f44f54d87f3a
Senalov <senalovaziz12@gmail.com>
2023-08-10 16:35:43 +0600

    error Unable to find the session to touch

--- server/pages/router.js
+++ server/pages/router.js
@@ -5,9 +5,21 @@ const User = require('../auth/User')
 const Blog = require('../Blogs/blog')
 
 router.get('/' , async(req, res) => {
+    const options = {}
+    const categ = await Genres.findOne({key : req.query.category})
+    if(categ){
+        options.category = categ._id
+        res.locals.category = req.query.category
+    }
+    let page = 0
+    const limit = 3
+    if(req.query.page && req.query.page > 0){
+        page = req.query.page
+    }
+    const totalBlogs = await Blog.count(options)
     const allGenres = await Genres.find()
-    const blogs = await Blog.find().populate('category').populate('author')
-    res.render("index" , {genres: allGenres , user: req.user ? req.user: {} , blogs})
+    const blogs = await Blog.find(options).limit(limit).skip(page * limit).populate('category').populate('author')
+    res.render("index" , {genres: allGenres , user: req.user ? req.user: {} , blogs , pages : Math.ceil(totalBlogs / limit)})
 })
 
 router.get('/profile/:id' , async(req, res) => {
@@ -39,9 +51,11 @@ router.get('/edit/:id' , async(req, res) => {
     res.render("edit" , {user: req.user ? req.user: {} , genres: allGenres , blog})
 })
 
-router.get('/comment' , async(req, res) => {
+router.get('/comment/:id' , async(req, res) => {
+    const user = await User.findById(req.params.id)
+    const blogs = await Blog.find().populate('category').populate('author')
     const allGenres = await Genres.find()
-    res.render("comment" , {genres: allGenres , user: req.user ? req.user: {}})
+    res.render("comment" , {user: user , user: req.user ? req.user: {} , genres: allGenres , loginUser: req.user , blogs: blogs})
 })
 
 router.get('/not-account' , async(req, res) => {
